c7e1835a5326f8cbfb531fdb82fa5ca7bbea8e89
bailsfrancisco <39094944>
2020-03-24 12:08:28 -0300

    Calculo de distancia, falta ordenarlo

--- app/src/main/java/ar/edu/unnoba/tpfinalppc/ClienteDetail.java
+++ app/src/main/java/ar/edu/unnoba/tpfinalppc/ClienteDetail.java
@@ -1,11 +1,17 @@
 package ar.edu.unnoba.tpfinalppc;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,30 +27,46 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import ar.edu.unnoba.tpfinalppc.Model.Cliente;
 
 public class ClienteDetail extends AppCompatActivity  implements OnMapReadyCallback {
 
-    TextView descripcion,detalle,distancia,domicilio, latitud, longitud,telefono,valor, tipo;
+    private static final String TAG = ClienteDetail.class.getSimpleName();
+
+    TextView descripcion,detalle,distancia,domicilio, telefono,valor, tipo;
     ImageView img_anonima;
     Cliente cliente;
 
     private GoogleMap googleMap;
     private MapView mMapView;
 
-    private static final String TAG = ClienteDetail.class.getSimpleName();
+    private LatLng location_cliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_details);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
+
         descripcion = findViewById(R.id.descripcionWebService);
         detalle = findViewById(R.id.detalleWebService);
         distancia = findViewById(R.id.distanciaWebService);
         domicilio = findViewById(R.id.domicilioWebService);
-        //latitud = findViewById(R.id.latitudWebService);
-        //longitud = findViewById(R.id.longitudWebService);
         telefono = findViewById(R.id.telefonoWebService);
         valor = findViewById(R.id.valorWebService);
         tipo = findViewById(R.id.tipoWebService);
@@ -53,17 +75,21 @@ public class ClienteDetail extends AppCompatActivity  implements OnMapReadyCallb
         Gson gson = new Gson();
         cliente = gson.fromJson(getIntent().getStringExtra("myjson"), Cliente.class);
 
+        location_cliente = new LatLng(cliente.getLatitud(), cliente.getLongitud());
+
+
         img_anonima.setImageResource(cliente.getImage());
         descripcion.setText(cliente.getDescripcion());
         detalle.setText(cliente.getDetalle());
+
+        /*
         if(cliente.getDistancia()>1000){
             distancia.setText(String.format("%.1f",cliente.getDistancia()/1000)+" Km");
         }else {
             distancia.setText(String.format("%.1f",cliente.getDistancia()) + " m");
-        }
+        }*/
+
         domicilio.setText(cliente.getDomicilio());
-        //latitud.setText(String.valueOf(cliente.getLatitud()));
-        //longitud.setText(String.valueOf(cliente.getLongitud()));
         telefono.setText("+"+cliente.getTelefono());
         valor.setText(String.valueOf(cliente.getValor()));
         tipo.setText(cliente.getTipo());
@@ -71,6 +97,20 @@ public class ClienteDetail extends AppCompatActivity  implements OnMapReadyCallb
         mMapView = findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
+    }
+
+    private float getDistance(LatLng usuario,LatLng cliente){
+        Location l1=new Location("One");
+        l1.setLatitude(usuario.latitude);
+        l1.setLongitude(usuario.longitude);
+
+        Location l2=new Location("Two");
+        l2.setLatitude(cliente.latitude);
+        l2.setLongitude(cliente.longitude);
+
+        float distance=l1.distanceTo(l2);
+
+        return distance;
     }
 
     @Override
@@ -163,4 +203,111 @@ public class ClienteDetail extends AppCompatActivity  implements OnMapReadyCallb
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
+
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setClienteDetail(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+
+    public void setLocation(Location loc) {
+        LatLng location_usuario = null;
+        float dist;
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    //Address DirCalle = list.get(0);
+                    Double lat = loc.getLatitude();
+                    Double lon = loc.getLongitude();
+
+                    location_usuario = new LatLng(lat, lon);
+                    //Devuelve la distancia entre dos puntos
+                    dist = getDistance(location_usuario, location_cliente);
+
+                    if(dist>1000){
+                        distancia.setText(String.format("%.1f",dist/1000)+" Km");
+                    }else {
+                        distancia.setText(String.format("%.1f",dist) + " m");
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class Localizacion implements LocationListener {
+
+        ClienteDetail clienteDetail;
+
+        public ClienteDetail getClienteDetail() {
+            return clienteDetail;
+        }
+
+        public void setClienteDetail(ClienteDetail clienteDetail) {
+            this.clienteDetail = clienteDetail;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas debido a la deteccion de un cambio de ubicacion del dispositivo
+
+            loc.getLatitude();
+            loc.getLongitude();
+
+            this.clienteDetail.setLocation(loc);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(TAG, "GPS Desactivado.");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(TAG, "GPS Activado.");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
+    }
+
 }
