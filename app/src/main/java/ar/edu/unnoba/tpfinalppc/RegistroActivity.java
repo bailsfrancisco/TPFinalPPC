dcfc1c9bdbc56c08cd40fb94fddf0c70c27807f8
bailsfrancisco <39094944>
2020-05-20 11:36:03 -0300

    Validar mail, comente locationStart en onCreate del MainActivity FIX regex email

--- app/src/main/java/ar/edu/unnoba/tpfinalppc/RegistroActivity.java
+++ app/src/main/java/ar/edu/unnoba/tpfinalppc/RegistroActivity.java
@@ -63,7 +63,7 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
 
     private void register() {
         String email = etEmail.getText().toString();
-        if (!email.matches("[a-zA-Z0-9._-]+@[az]+.[az]+")) {
+        if (!email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
             etEmail.setError("Ingrese un email valido");
         }else {
             String pass = etPassword.getText().toString();
